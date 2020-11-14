ec2ab4b94729b46d97e5fff7d0e887b5388a1772
trjh2k2 <trjh2k2@gmail.com>
2020-11-13 23:06:39 -0500

    Adding first pass / example of MIDI playing plugin

---
+++ Midi Monsters/Assets/Scripts/Plugins/TH_Audio/TH_Audio.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.Assertions;
+
+public class TH_Audio : MonoBehaviour
+{
+    [DllImport("TH_Audio.dll")]
+    private static extern void Init();
+
+    [DllImport("TH_Audio.dll")]
+    private static extern void Shutdown();
+
+    [DllImport("TH_Audio.dll")]
+    private static extern int LoadMidi(string path);
+
+    [DllImport("TH_Audio.dll")]
+    private static extern void PlayMidi(int id, bool loop);
+
+    void Awake()
+    {
+        TH_Audio.Init();
+    }
+
+    void OnDestroy()
+    {
+        TH_Audio.Shutdown();
+    }
+
+    public class MIDISound
+    {
+        private int handle = -1;
+        public MIDISound(string path)
+        {
+            Debug.Assert(!string.IsNullOrEmpty(path), "Midi file path cannot be empty.");
+            handle = LoadMidi(path);
+        }
+
+        public void Play(bool looping = false)
+        {
+            PlayMidi(handle, looping);
+        }
+    }
+
+}
---
+++ Midi Monsters/Assets/Scripts/Testing/TomsMidiTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomsMidiTest : MonoBehaviour
+{
+    private TH_Audio.MIDISound bgm;
+    private TH_Audio.MIDISound bloop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm = new TH_Audio.MIDISound("Assets\\MIDI\\loop1.mid");
+        bloop = new TH_Audio.MIDISound("Assets\\MIDI\\bloop.mid");
+
+        bgm.Play(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            bloop.Play();
+        }
+    }
+}
